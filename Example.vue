59ead0645372907a6e10a686546bb2f6296f6b36
Rahul <85161282+500apps-RahulVangari@users.noreply.github.com>
2021-07-05 18:28:21 +0530

    Add files via upload

---
+++ Example.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>{{exm}}</p>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            exm:[],
+        }
+    },
+    methods:{
+        async fetchQuestions(){
+            const res = await fetch('http://localhost:5000/fill_exam');
+            const data = await res.json();
+            return data
+        },
+        async created(){
+            this.exm = await this.fetchQuestions();
+        }
+    }
+}
+</script>
