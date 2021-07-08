b48852d57c2e4afd59befc48f92bcf070f69dcff
Rahul <85161282+500apps-RahulVangari@users.noreply.github.com>
2021-07-08 18:45:46 +0530

    Update List.vue

--- Mixins and axios/List.vue
+++ Mixins and axios/List.vue
@@ -1,15 +1,28 @@
 <template>
- <div>
-     <input type="text" v-model="filtertext">
+<div>
+ <div class="row">
+     <h2>List</h2>
+     <p v-html="'<strong>Strong v-html</strong>'"></p>
+     <p v-text="'V-text'"></p>
+     <!-- <input type="text" v-model="filtertext">
      <ul>
     <li v-for="f in filtering" :key="f">{{f}}</li>
-  </ul>
+  </ul> -->
  </div>
+<div class="row">
+    <h1 v-highlight:background="'aqua'">Custom directive</h1>
+</div>
+</div>
 </template>
 
 <script>
-import { frMixins } from "./frMixins";
+// import { frMixins } from "./frMixins";
 export default {
-    mixins:[frMixins]
+    // mixins:[frMixins]
 }
 </script>
+<style scoped>
+/* .row{
+    background-color:;
+} */
+</style>
