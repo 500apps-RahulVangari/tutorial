b32e9da036efc5b2c8ded28e80659982f8b536bf
Rahul <85161282+500apps-RahulVangari@users.noreply.github.com>
2021-07-08 18:46:10 +0530

    Update main.js

--- Mixins and axios/main.js
+++ Mixins and axios/main.js
@@ -1,19 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {BootstrapVue,IconsPlugin} from 'bootstrap-vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
-
+Vue.directive('highlight',{
+  bind(el,binding){
+    el.style.backgroundColor="navy";
+    el.style.color="honeydew";
+    el.style.backgroundColor=binding.value;
+  }
+})
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.filter('tolower',function(val){
   return val.toLowerCase()
 })
 
+axios.defaults.baseURL="https://fill-70ce3-default-rtdb.firebaseio.com"
+
 Vue.mixin({
-  created(){
-    console.log('Global Mixins')
-  }
+  // created(){
+  //   console.log('Global Mixins')
+  // }
 })
 
 import 'bootstrap/dist/css/bootstrap.css'
